b6068fff8a8e65dc87f871b918c4dd2eb3782dd5
Carlos Villasenor <carlosdeveloper9@gmail.com>
2026-07-22 23:01:01 -0600

    feat: add next level food project to project section

--- src/components/Content/Projects/ProjectsData.tsx
+++ src/components/Content/Projects/ProjectsData.tsx
@@ -6,5 +6,12 @@ export const projects = [
     technologies: ["Javascript", "Typescript"],
     website: "https://legacy.videojs.org/guides/spatial-navigation/",
     image: "spatial-navigation.png"
+  },
+  {
+    title: "NextLevel Food",
+    description: "NextLevel Food is a platform for foodies to share their favorite recipes with the world. It's a place to discover new dishes, and to connect with other food lovers.",
+    technologies: ["React", "Next.js", "Tailwind CSS", "BetterSQLite"],
+    website: "https://onwards-foodies-project.vercel.app/",
+    image: "nextlevel-food.png"
   }
 ];
