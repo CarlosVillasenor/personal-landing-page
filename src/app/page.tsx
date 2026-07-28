3526a79781da3aea350919e7719b93cc3ba0d16e
Carlos Villasenor <carlosdeveloper9@gmail.com>
2026-07-27 19:12:43 -0600

    feat: implement mobile header component and hook to only show it on mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import Header from "@/components/Header/Header";
 import Content from "@/components/Content/Content";
+import MobileHeader from "@/components/MobileHeader/MobileHeader";
 import styles from "./page.module.css";
+import { useIsDesktop } from "@/hooks/useIsDesktop";
 
 export default function Home() {
+  const isDesktop = useIsDesktop();
+  const showMobileHeader = !isDesktop;
+
   return (
     <main className={styles.main}>
+      {showMobileHeader && <MobileHeader />}
       <Header />
       <Content />
     </main>
--- src/components/MobileHeader/MobileHeader.tsx
+++ src/components/MobileHeader/MobileHeader.tsx
@@ -7,6 +7,8 @@ import styles from "./MobileHeader.module.css";
 export default function MobileHeader() {
 	const activeSection = useActiveSection();
 
+  console.log("activeSection", activeSection);
+
 	return (
 		<header className={styles.wrapper}>
 			<nav className={styles.nav} aria-label="Primary sections">
